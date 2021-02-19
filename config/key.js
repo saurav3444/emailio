aa711c50d61b1038bcc9a821cf227593aeb37cde
Saurav Kumar <saurav.kumar3@publicissapient.com>
2021-02-19 13:01:03 +0530

    finished auth flow

---
+++ config/key.js
@@ -0,0 +1,12 @@
+//key.js -> return dev or prod
+
+if (process.env.NODE_ENV === 'production') {
+
+    //in production
+    module.exports = require('./prod');
+
+} else {
+
+    //in dev
+    module.exports = require('./dev');
+}
